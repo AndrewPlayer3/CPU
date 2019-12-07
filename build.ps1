0d8d05b6e6ace5c367678023b311c567d9e521e0
Andrew Player <andrewplayer3@gmail.com>
2019-12-07 06:37:44 -0900

    instructions in the build file

--- build.ps1
+++ build.ps1
@@ -11,6 +11,9 @@ mkdir bin -erroraction 'silentlycontinue' | Out-Null
 Remove-Item bin/cpu.exe -erroraction 'silentlycontinue'
 Remove-Item bin/assembler.exe -erroraction 'silentlycontinue'
 
+# New Line
+Write-Output ""
+ 
 # Compile to bin/cpu
 g++ -std=c++17 -O3 src/cpu_main.cpp src/instr.cpp -o bin/cpu
 Write-Output "Compiled cpu to bin/cpu"
@@ -18,3 +21,11 @@ Write-Output "Compiled cpu to bin/cpu"
 # Compile to bin/assembler
 g++ -std=c++17 -O3 src/assembler_main.cpp src/assembler.cpp -o bin/assembler
 Write-Output "Compiled assembler to bin/assembler"
+
+# Print some instructions
+Write-Output ""
+Write-Output "To assemble the test files use this: 'bin/assembler filename'"
+Write-Output "To run the test files use this: 'bin/cpu filename'"
+Write-Output "Note: the cpu only runs the .inst files. These are what the assembler generates."
+Write-Output "Check the programs folder for the test programs."
+Write-Output ""
--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ mkdir bin 2>/dev/null
 rm bin/cpu.exe 2>/dev/null
 rm bin/assembler.exe 2>/dev/null
 
+# New Line
+echo ""
+
 # Compile to bin/cpu
 g++ -std=c++17 -O3 src/cpu_main.cpp src/instr.cpp -o bin/cpu
 echo "Compiled cpu to bin/cpu"
@@ -19,3 +22,11 @@ echo "Compiled cpu to bin/cpu"
 # Compile to bin/assembler
 g++ -std=c++17 -O3 src/assembler_main.cpp src/assembler.cpp -o bin/assembler
 echo "Compiled assembler to bin/assembler"
+
+# Print some instructions
+echo ""
+echo "To assemble the test files use this: \"bin/assembler filename\""
+echo "To run the test files use this: \"bin/cpu filename.inst\""
+echo "Note: the cpu only runs the .inst files. These are what the assembler generates"
+echo "Check the programs folder for the test programs."
+echo ""
