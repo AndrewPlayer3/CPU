a1425c74f8559d7ac7ff53cb3a88bbf8b4adfb93
Andrew Player <asplayer@alaska.edu>
2019-12-06 16:46:11 -0900

    compilation scripts

--- build.ps1
+++ build.ps1
@@ -1,4 +1,18 @@
+# This script compiles the cpu emulator and assembler
+# they are compiled to the bin/ directory
+
+# Create bin directory if it does not exist
+# Dont print the "it already exists" error
+mkdir bin -erroraction 'silentlycontinue'
+
+# Remove the the existing execs just because
+Remove-Item bin/cpu.exe
+Remove-Item bin/assembler.exe
+
+# Compile to bin/cpu
 g++ -std=c++17 -O3 src/cpu_main.cpp src/instr.cpp -o bin/cpu
 Write-Output "Compiled cpu to bin/cpu"
+
+# Compile to bin/assembler
 g++ -std=c++17 -O3 src/assembler_main.cpp src/assembler.cpp -o bin/assembler
 Write-Output "Compiled assembler to bin/assembler"
--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+
+# This script compiles the cpu emulator and assembler
+# they are compiled to the bin/ directory
+
+# Create bin directory if it does not exist
+# Dont print the "it already exists" error
+mkdir bin 2>/dev/null 
+
+# Remove the the existing execs just because
+rm bin/cpu.exe
+rm bin/assembler.exe
+
+# Compile to bin/cpu
 g++ -std=c++17 -O3 src/cpu_main.cpp src/instr.cpp -o bin/cpu
 echo "Compiled cpu to bin/cpu"
+
+# Compile to bin/assembler
 g++ -std=c++17 -O3 src/assembler_main.cpp src/assembler.cpp -o bin/assembler
 echo "Compiled assembler to bin/assembler"
