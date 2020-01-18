47d32284e8f595cbb84e690b2cad7b773c5c8c11
Andrew Player <andrewplayer3@gmail.com>
2020-01-17 23:44:17 -0900

    c++ instead of g++

--- build.sh
+++ build.sh
@@ -16,11 +16,11 @@ rm bin/assembler.exe 2>/dev/null
 echo ""
 
 # Compile to bin/cpu
-g++ -std=c++17 -O3 src/cpu_main.cpp src/instr.cpp -o bin/cpu
+c++ -std=c++17 -O3 src/cpu_main.cpp src/instr.cpp -o bin/cpu
 echo "Compiled cpu to bin/cpu"
 
 # Compile to bin/assembler
-g++ -std=c++17 -O3 src/assembler_main.cpp src/assembler.cpp -o bin/assembler
+c++ -std=c++17 -O3 src/assembler_main.cpp src/assembler.cpp -o bin/assembler
 echo "Compiled assembler to bin/assembler"
 
 # Print some instructions
