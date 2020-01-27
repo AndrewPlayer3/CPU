6a9106a13c0f86d38c3e407fa11ee1c9c6727fa4
Andrew Player <andrewplayer3@gmail.com>
2020-01-26 17:01:02 -0900

    removed main from gitignore

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include "instr.hpp"
+#include "assembler.hpp"
+
+int main(int argc, char* argv[]) {
+    if(argv[1] == nullptr) {
+        std::cout << "\nERROR: Please enter a valid filename along with the"
+                  << " executable in the command line. " 
+                  << "\ni.e. bin/assembler filename\n" 
+                  << "P.S. The test assembly files are in programs/  the ones WITHOUT .inst\n" << std::endl;
+            return 1;
+    }
+    std::cout << std::endl;
+    std::string filename = argv[1];
+    std::ostringstream machine_code = gen_machine_code(filename);
+    std::string ofilename = filename + ".inst";
+    std::ofstream out_file(ofilename);
+    out_file << machine_code.str();
+    std::cout << "Program Compiled Successfully. Made: " << ofilename << std::endl;
+    out_file.close();
+    filename = argv[1] + std::string(".inst");
+    CPU cpu;
+    if(!parse_file(filename, &cpu.mem[0], cpu.next_free_location, cpu.end_text_section)) {
+        std::cout << "Error opening file: " << filename << std::endl;
+    }
+    std::cout << std::endl;
+    cpu.run();
+    std::cout << "\nProgram Exited Successfully :)" << std::endl;
+    std::cout << std::endl;
+    return 0;
+}
